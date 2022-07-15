4ccbf046d994f6f13e289940a7438776c1d045d9
范文杰 <fanwenjie.fe@bytedance.com>
2022-07-15 20:05:28 +0800

    add examples

--- minify-html/webpack.config.js
+++ minify-html/webpack.config.js
@@ -16,19 +16,19 @@ module.exports = {
     minimizer: [
       // Webpack5 之后，约定使用 `'...'` 字面量保留默认 `minimizer` 配置
       "...",
-      // new HtmlMinimizerPlugin({
-      //   minimizerOptions: {
-      //     // 折叠 Boolean 型属性
-      //     collapseBooleanAttributes: true,
-      //     // 使用精简 `doctype` 定义
-      //     useShortDoctype: true,
-      //     // ...
-      //   },
-      // }),
+      new HtmlMinimizerPlugin({
+        minimizerOptions: {
+          // 折叠 Boolean 型属性
+          collapseBooleanAttributes: true,
+          // 使用精简 `doctype` 定义
+          useShortDoctype: true,
+          // ...
+        },
+      }),
     ],
   },
   plugins: [
-    // 简单期间，这里我们使用 `html-webpack-plugin` 自动生成 HTML 演示文件
+    // 简单起见，这里我们使用 `html-webpack-plugin` 自动生成 HTML 演示文件
     new HtmlWebpackPlugin({
       templateContent: `<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
     <html>
