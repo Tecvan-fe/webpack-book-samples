050ed93871931dafd8d3994d0160fb07d0959706
范文杰 <fanwenjie.fe@bytedance.com>
2022-07-16 00:12:47 +0800

    add example

---
+++ minify-css/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  devtool: false,
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /.css$/,
+        // 注意，这里用的是 `MiniCssExtractPlugin.loader` 而不是 `style-loader`
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      // Webpack5 之后，约定使用 `'...'` 字面量保留默认 `minimizer` 配置
+      "...",
+      // new CssMinimizerPlugin(),
+    ],
+  },
+  // 需要使用 `mini-css-extract-plugin` 将 CSS 代码抽取为单独文件
+  // 才能命中 `css-minimizer-webpack-plugin` 默认的 `test` 规则
+  plugins: [new MiniCssExtractPlugin()],
+};
