a24e97c14dc7abf369a50bcae199e44245d6eb2c
范文杰 <fanwenjie.fe@bytedance.com>
2022-07-15 20:01:07 +0800

    add examples

---
+++ minify-html/src/index.js
@@ -0,0 +1,15 @@
+console.log("hello world");
+
+let a = "wfweefwe";
+let b = a + "fwefwe";
+let c = 3;
+
+alert(a, b, c);
+a = a + "fwe";
+add(a, b, c);
+
+(() => {
+  alert("cds");
+  let a = "fwefwe";
+  alert(a + 3);
+})();
---
+++ minify-html/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  devtool: false,
+  entry: { foo: "./src/index.js" },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      // Webpack5 之后，约定使用 `'...'` 字面量保留默认 `minimizer` 配置
+      "...",
+      // new HtmlMinimizerPlugin({
+      //   minimizerOptions: {
+      //     // 折叠 Boolean 型属性
+      //     collapseBooleanAttributes: true,
+      //     // 使用精简 `doctype` 定义
+      //     useShortDoctype: true,
+      //     // ...
+      //   },
+      // }),
+    ],
+  },
+  plugins: [
+    // 简单期间，这里我们使用 `html-webpack-plugin` 自动生成 HTML 演示文件
+    new HtmlWebpackPlugin({
+      templateContent: `<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+    <html>
+      <head>
+        <meta charset="UTF-8" />
+        <title>webpack App</title>
+      </head>
+      <body>
+        <input readonly="readonly"/>
+        <!-- comments -->
+        <script src="index_bundle.js"></script>
+      </body>
+    </html>`,
+    }),
+  ],
+};
